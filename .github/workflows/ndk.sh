a79745579f2bd19d31341044ccf5aa1508270ed1
nn-firc <53158768+nn-firc@users.noreply.github.com>
2023-01-25 13:48:34 +0800

    override

--- .github/workflows/ndk.sh
+++ .github/workflows/ndk.sh
@@ -3,7 +3,8 @@ wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang
 tar xpvf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
 unzip android-ndk-r10e-linux-x86_64.zip
-export ANDROID_NDK=$PWD/android-ndk-r10e
+export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
+export NDK_HOME=$PWD/android-ndk-r10e/
 export PATH=$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
 ./waf configure -T release --64bits --android=aarch64,host,21 --prefix=/home/runner/work/csgo-sdk-2017/csgo-sdk-2017/csgo-android
 ./waf install -j4
