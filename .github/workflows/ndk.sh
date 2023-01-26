0c5298db801ebf8fbbd72955f2aa010e307d764d
nn-firc <53158768+nn-firc@users.noreply.github.com>
2023-01-26 17:44:25 +0800

    [workflows]update clang
    may faster

--- .github/workflows/ndk.sh
+++ .github/workflows/ndk.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
-tar xpvf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10.tar.xz -o /dev/null
+tar xpvf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10.tar.xz
 wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
 unzip android-ndk-r10e-linux-x86_64.zip
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export NDK_HOME=$PWD/android-ndk-r10e/
-export PATH=$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH
+export PATH=$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-20.10/bin:$PATH
 ./waf configure -T release --64bits --togles --android=aarch64,host,21 --prefix=/home/runner/work/csgo-sdk-2017/csgo-sdk-2017/csgo-android
 ./waf install -j4 --strip
 
